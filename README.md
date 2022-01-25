fc9d0e99d16b7187c3e533218c1989f95c48b268
Do Ngoc Khanh <14163153+ngockhanhdo1997@users.noreply.github.com>
2022-01-25 14:58:22 +0900

    Support continuous type and all wordsizes

--- README.md
+++ README.md
@@ -7,6 +7,3 @@ This is a tool for converting WRF/WPS geogrid binary format to GeoTIFF.
 ```
 python geogrid_to_tif.py WPS_GEOG/modis_20class_30s_with_lakes/ modis_20class_30s_with_lakes.tif
 ```
-
-Currently only categorical, 1 byte, two-dimensional dataset is supported.
-Support for other format might be done in the future.
--- geogrid_to_tif.py
+++ geogrid_to_tif.py
@@ -4,12 +4,14 @@ import sys
 
 import numpy as np
 from affine import Affine
+from numba import njit
 from osgeo import gdal
 from osgeo import osr
+from tqdm import tqdm
 
 
 def read_index(index_path):
-    int_fields = [
+    int_fields = {
         "category_min",
         "category_max",
         "tile_x",
@@ -17,9 +19,28 @@ def read_index(index_path):
         "tile_z",
         "wordsize",
         "filename_digits",
-    ]
-    float_fields = ["dx", "dy", "known_x", "known_y", "known_lat", "known_lon"]
-    index = {"filename_digits": 5, "tile_z": 1}
+        "tile_bdr",
+    }
+    float_fields = {
+        "dx",
+        "dy",
+        "known_x",
+        "known_y",
+        "known_lat",
+        "known_lon",
+        "scale_factor",
+        "missing_value",
+    }
+    boolean_fields = {"signed"}
+    index = {
+        "filename_digits": 5,
+        "signed": False,
+        "known_x": 1,
+        "known_y": 1,
+        "scale_factor": 1,
+        "endian": "big",
+        "tile_bdr": 0,
+    }
 
     with open(index_path) as fp:
         for line in fp:
@@ -33,6 +54,8 @@ def read_index(index_path):
                 v = int(v)
             elif k in float_fields:
                 v = float(v)
+            elif k in boolean_fields:
+                v = v == "yes"
             elif v[0] == '"' and v[-1] == '"':
                 v = v[1:-1]
             index[k] = v
@@ -40,12 +63,34 @@ def read_index(index_path):
     return index
 
 
+def get_gdal_type(wordsize, signed):
+    if signed:
+        return [gdal.GDT_Int16, gdal.GDT_Int16, gdal.GDT_Int32, gdal.GDT_Int32][
+            wordsize - 1
+        ]
+    return [gdal.GDT_Byte, gdal.GDT_UInt16, gdal.GDT_UInt32, gdal.GDT_UInt32][
+        wordsize - 1
+    ]
+
+
+@njit
+def decode_geogrid_binary(binary_str, wordsize, signed):
+    arr = []
+    for start in range(0, len(binary_str), wordsize):
+        num = 0
+        for i in range(0, wordsize):
+            num = (num << 8) | binary_str[start + i]
+        if signed and num >= (1 << (8 * wordsize - 1)):
+            num -= 1 << (8 * wordsize)
+        arr.append(num)
+    return np.array(arr)
+
+
 def main(geog_dir, output_path):
     index = read_index(os.path.join(geog_dir, "index"))
 
-    assert index["type"] == "categorical"
-    assert index["wordsize"] == 1
-    assert index["tile_z"] == 1
+    assert index["projection"] == "regular_ll"
+    assert index["dx"] > 0
 
     if index["filename_digits"] == 5:
         tile_paths = glob.glob(os.path.join(geog_dir, "?????-?????.?????-?????"))
@@ -77,9 +122,9 @@ def main(geog_dir, output_path):
         output_path,
         x_size,
         y_size,
-        1,
-        gdal.GetDataTypeByName("Byte"),
-        options=["COMPRESS=LZW"],
+        index["tile_z"],
+        get_gdal_type(index["wordsize"], index["signed"]),
+        options=["COMPRESS=LZW", "TILED=YES"],
     )
 
     sr = osr.SpatialReference()
@@ -91,22 +136,52 @@ def main(geog_dir, output_path):
         * Affine.scale(index["dx"], index["dy"])
         * Affine.translation(-index["known_x"], -index["known_y"])
     )
-    ulx, uly = affine * (1, y_size)
-    ds.SetGeoTransform([ulx, index["dx"], 0, uly, 0, -index["dy"]])
 
-    band = ds.GetRasterBand(1)
+    is_top_to_bottom = index["dy"] < 0
 
-    for tile_path, (x_start, _, _, y_end) in zip(tile_paths, tile_bounds):
+    if is_top_to_bottom:
+        ulx, uly = affine * (1, 1)
+        ds.SetGeoTransform([ulx, index["dx"], 0, uly, 0, index["dy"]])
+    else:
+        ulx, uly = affine * (1, y_size)
+        ds.SetGeoTransform([ulx, index["dx"], 0, uly, 0, -index["dy"]])
+
+    bands = [ds.GetRasterBand(i + 1) for i in range(index["tile_z"])]
+    for band in bands:
+        band.SetScale(index["scale_factor"])
+        if "missing_value" in index:
+            band.SetNoDataValue(index["missing_value"])
+
+    for tile_path, (x_start, _, y_start, y_end) in tqdm(
+        list(zip(tile_paths, tile_bounds))
+    ):
         with open(tile_path, "rb") as fp:
-            tile = np.frombuffer(fp.read(), dtype="u1")
-        tile = tile.reshape((index["tile_y"], index["tile_x"]))[::-1]
-        band.WriteRaster(
-            int(x_start - 1),
-            int(y_size - y_end),
-            index["tile_x"],
-            index["tile_y"],
-            tile,
+            tile = decode_geogrid_binary(fp.read(), index["wordsize"], index["signed"])
+        tile = tile.reshape(
+            (
+                index["tile_z"],
+                index["tile_y"] + 2 * index["tile_bdr"],
+                index["tile_x"] + 2 * index["tile_bdr"],
+            )
         )
+        if index["tile_bdr"] > 0:
+            tile = tile[
+                :,
+                index["tile_bdr"] : -index["tile_bdr"],
+                index["tile_bdr"] : -index["tile_bdr"],
+            ]
+        if is_top_to_bottom:
+            x_off, y_off = x_start - 1, y_start - 1
+        else:
+            x_off, y_off = x_start - 1, y_size - y_end
+            tile = tile[:, ::-1]
+        for band, z_slice in zip(bands, tile):
+            band.WriteRaster(
+                int(x_off), int(y_off), index["tile_x"], index["tile_y"], z_slice
+            )
+
+    bands = None
+    ds = None
 
 
 if __name__ == "__main__":
